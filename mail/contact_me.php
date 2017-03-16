a1b50c3d0162caab7ed6fb903be411fe4a34d831
kevlarcade <kevlarcade@gmail.com>
2017-03-16 15:53:46 +0100

    rajout du support utf8
    Signed-off-by: kevlarcade <kevlarcade@gmail.com>

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,5 @@
 <?php
+mb_internal_encoding("UTF-8");
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -21,6 +22,6 @@ $email_body = "Vous avez recu un message depuis le formulaire du site.\n\n"."Voi
 $headers = "From: noreply@labolyon.fr\n";
 $headers .= "Reply-To: $email_address";	
 $headers .= "Date: ".date("r");	
-mail($to,utf8_encode($email_subject),utf8_encode($email_body),$headers);
+mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
